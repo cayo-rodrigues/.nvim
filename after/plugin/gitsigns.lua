eb49d974bce063ddc4b63277991a31b5e80b3d5c
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2023-07-30 15:10:04 -0300

    improve some keymaps

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -16,13 +16,13 @@ require('gitsigns').setup({
         end
 
         -- Navigation
-        map('n', ']c', function()
+        map({ 'n', 'v' }, ']c', function()
             if vim.wo.diff then return ']c' end
             vim.schedule(function() gs.next_hunk() end)
             return '<Ignore>'
         end, { expr = true })
 
-        map('n', '[c', function()
+        map({ 'n', 'v' }, '[c', function()
             if vim.wo.diff then return '[c' end
             vim.schedule(function() gs.prev_hunk() end)
             return '<Ignore>'
