584c48fd21013f5cf2df0bb01bcecc8a3dfacbd9
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2024-01-18 21:23:56 -0300

    treesitter stuff (must install tree-sitter-cli)

--- lua/conf/treesitter.lua
+++ lua/conf/treesitter.lua
@@ -4,12 +4,12 @@
 require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
-      'bash', 'yaml', 'json', 'html', 'css' },
+      'bash', 'yaml', 'json', 'html', 'css', 'sql', 'query' },
 
     sync_install = false,
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = false,
+    auto_install = true,
 
 
     highlight = {
