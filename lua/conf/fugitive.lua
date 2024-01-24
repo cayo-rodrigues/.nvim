bb141dd18adfd94d7a4d4d3eedc4f1989b75d2ea
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2024-01-24 09:17:44 -0300

    add missing <CR>

--- lua/conf/fugitive.lua
+++ lua/conf/fugitive.lua
@@ -3,9 +3,9 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gh", "<cmd>diffget //2<CR>", { desc = "choose left side of conflict" })
 vim.keymap.set("n", "<leader>gl", "<cmd>diffget //3<CR>", { desc = "choose right side of conflict" })
 
-vim.keymap.set("n", "<leader>gp", ":!gpush", { desc = "git push origin <current-branch-name>" })
+vim.keymap.set("n", "<leader>gp", ":!gpush<CR>", { desc = "git push origin <current-branch-name>" })
 
-vim.keymap.set("n", "<leader>gP", ":!gpull", { desc = "git pull origin <current-branch-name>" })
+vim.keymap.set("n", "<leader>gP", ":!gpull<CR>", { desc = "git pull origin <current-branch-name>" })
 
 local CayFugitive = vim.api.nvim_create_augroup("Cay_Fugitive", {})
 
