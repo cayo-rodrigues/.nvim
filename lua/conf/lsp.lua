502ec2d9ffe17a7a3419d6d582805c6b0ffa7d14
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2024-02-02 08:08:21 -0300

    that's beautiful

--- lua/conf/lsp.lua
+++ lua/conf/lsp.lua
@@ -126,6 +126,7 @@ local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
+luasnip.filetype_extend("templ", { "html" })
 
 cmp.setup {
     snippet = {
