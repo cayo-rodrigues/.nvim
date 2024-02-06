3617489ca51d3442f0c8cde564845e5c35974ba1
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2024-02-06 12:51:16 -0300

    use tokyonight-night instead of default tokyonight

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -4,12 +4,7 @@ return {
     config = function()
         require('tokyonight').setup({
             lualine_bold = true,
-            on_colors = function(colors)
-                colors.bg = "#222230"
-                colors.bg_statusline = "#1e1e2e"
-                colors.fg_gutter = "#444b6a"
-            end
         })
-        vim.cmd.colorscheme 'tokyonight'
+        vim.cmd.colorscheme 'tokyonight-night'
     end
 }
